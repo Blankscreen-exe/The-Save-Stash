f9c388e600240be4365f3a64f1a4992d621122f1
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-12-24 17:55:59 +0500

    fixed myself a table

---
+++ table.js
@@ -0,0 +1,29 @@
+var base_url = "https://raw.githubusercontent.com/Blankscreen-exe/The-Save-Stash/refs/heads/main/";
+var data_url = base_url + "data.json";
+
+$(document).ready(function () {
+  $("#saveStashTable").DataTable({
+    ajax: data_url,
+    columns: [
+      {
+        data: "game",
+        render: function (data, type) {
+          return `<strong>${data}</strong>`;
+        },
+      },
+      {
+        data: "platform",
+        render: function (data, type) {
+          return `<span>${data.toUpperCase()}</span>`;
+        },
+      },
+      {
+        data: "fileName",
+        render: function (data, type, row) {
+          console.log(row)
+          return `<a href="${base_url+row.platform+"/"+row.game+"/"+row.fileName}" class="save-file-link">${data}</span>`;
+        },
+      },
+    ],
+  });
+});
