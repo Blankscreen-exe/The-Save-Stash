4e8fbec754938e920498091178b158675f3d65bb
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-12-28 17:51:11 +0500

    changed game name formatting

--- table.js
+++ table.js
@@ -2,27 +2,39 @@ var base_url = "https://raw.githubusercontent.com/Blankscreen-exe/The-Save-Stash
 var data_url = base_url + "data.json";
 
 $(document).ready(function () {
-  $("#saveStashTable").DataTable({
-    ajax: data_url,
-    columns: [
-      {
-        data: "game",
-        render: function (data, type) {
-          return `<strong>${data}</strong>`;
-        },
-      },
-      {
-        data: "platform",
-        render: function (data, type) {
-          return `<span>${data.toUpperCase()}</span>`;
-        },
-      },
-      {
-        data: "fileName",
-        render: function (data, type, row) {
-          return `<a href="${base_url+"saves/"+row.platform+"/"+row.game+"/"+data}" class="save-file-link">${data}</span>`;
-        },
-      },
-    ],
+	
+	function formatNameString(input) {
+	  return input
+		.split('_')
+		.map((word) =>
+		  word === word.toUpperCase()
+			? word
+			: word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+		)
+		.join(' '); 
+	}
+
+	$("#saveStashTable").DataTable({
+	ajax: data_url,
+	columns: [
+	  {
+		data: "game",
+		render: function (data, type) {
+		  return `<strong>${formatNameString(data)}</strong>`;
+		},
+	  },
+	  {
+		data: "platform",
+		render: function (data, type) {
+		  return `<span>${data.toUpperCase()}</span>`;
+		},
+	  },
+	  {
+		data: "fileName",
+		render: function (data, type, row) {
+		  return `<a href="${base_url+"saves/"+row.platform+"/"+row.game+"/"+data}" class="save-file-link">${data}</span>`;
+		},
+	  },
+	],
   });
 });
